6edde20b9c695c2c3cd05e4e3c4ecf1afc6eeb2f
Craig Miller <craigmiller160@gmail.com>
2018-03-01 18:03:08 -0500

    Moved the exercise 1 components into new sub-directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './exercise1/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './exercise1/success-alert/success-alert.component';
 
 
 @NgModule({
