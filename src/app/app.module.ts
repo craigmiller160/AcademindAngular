5ce4e53e42f9b0145d1517e7028f3c7d51eb0927
Craig Miller <craigmiller160@gmail.com>
2018-03-01 21:27:51 -0500

    Fixed imports and built header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './older-lessons/lecture/server/server.component';
-import { ServersComponent } from './older-lessons/lecture/servers/servers.component';
-import { WarningAlertComponent } from './older-lessons/exercise1/warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './older-lessons/exercise1/success-alert/success-alert.component';
-import { TabsComponent } from './older-lessons/tabs/tabs.component';
-import { UsernameInterpolateComponent } from './older-lessons/exercise2/username-interpolate/username-interpolate.component';
-import { LogButtonClicksComponent } from './older-lessons/exercise3/log-button-clicks/log-button-clicks.component';
+import { ServerComponent } from './other-lessons/lecture/server/server.component';
+import { ServersComponent } from './other-lessons/lecture/servers/servers.component';
+import { WarningAlertComponent } from './other-lessons/exercise1/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './other-lessons/exercise1/success-alert/success-alert.component';
+import { TabsComponent } from './other-lessons/tabs/tabs.component';
+import { UsernameInterpolateComponent } from './other-lessons/exercise2/username-interpolate/username-interpolate.component';
+import { LogButtonClicksComponent } from './other-lessons/exercise3/log-button-clicks/log-button-clicks.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
