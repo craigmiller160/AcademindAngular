0c92763183f6a06b43b2fd537776826eb9039b8f
Craig Miller <craigmiller160@gmail.com>
2018-03-01 20:58:27 -0500

    Refactored all older lessons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './lecture/server/server.component';
-import { ServersComponent } from './lecture/servers/servers.component';
-import { WarningAlertComponent } from './exercise1/warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './exercise1/success-alert/success-alert.component';
-import { TabsComponent } from './tabs/tabs.component';
-import { UsernameInterpolateComponent } from './exercise2/username-interpolate/username-interpolate.component';
-import { LogButtonClicksComponent } from './exercise3/log-button-clicks/log-button-clicks.component';
+import { ServerComponent } from './older-lessons/lecture/server/server.component';
+import { ServersComponent } from './older-lessons/lecture/servers/servers.component';
+import { WarningAlertComponent } from './older-lessons/exercise1/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './older-lessons/exercise1/success-alert/success-alert.component';
+import { TabsComponent } from './older-lessons/tabs/tabs.component';
+import { UsernameInterpolateComponent } from './older-lessons/exercise2/username-interpolate/username-interpolate.component';
+import { LogButtonClicksComponent } from './older-lessons/exercise3/log-button-clicks/log-button-clicks.component';
+import { OlderLessonsComponent } from './older-lessons/older-lessons.component';
 
 
 @NgModule({
@@ -22,7 +23,8 @@ import { LogButtonClicksComponent } from './exercise3/log-button-clicks/log-butt
     SuccessAlertComponent,
     TabsComponent,
     UsernameInterpolateComponent,
-    LogButtonClicksComponent
+    LogButtonClicksComponent,
+    OlderLessonsComponent
   ],
   imports: [
     BrowserModule,
