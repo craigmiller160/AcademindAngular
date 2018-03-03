6458a7f4f0976aff11150a54d050a9f126fba8e8
craigmiller160 <craigmiller160@gmail.com>
2018-03-03 13:15:38 -0500

    Learned about HostListener and HostBinding

--- src/app/other-lessons/show-numbers/control/better-highlight/better-highlight.directive.ts
+++ src/app/other-lessons/show-numbers/control/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,26 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  // Can listen for any event from the element this directive sits on
+  @HostListener('mouseenter') mouseOver(eventData: Event): void {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event): void {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
