b36d63bb8fd21f5cdcea91a759bc82f3a43de791
craigmiller160 <craigmiller160@gmail.com>
2018-03-03 13:22:00 -0500

    Looked at ways to bind properties to directive

--- src/app/other-lessons/show-numbers/control/better-highlight/better-highlight.directive.ts
+++ src/app/other-lessons/show-numbers/control/better-highlight/better-highlight.directive.ts
@@ -1,26 +1,29 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @Input() defaultColor = 'transparent';
+  @Input('appBetterHighlight') highlightColor = 'blue';
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
   }
 
   // Can listen for any event from the element this directive sits on
   @HostListener('mouseenter') mouseOver(eventData: Event): void {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event): void {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
