56c25f2d17162bef58154a487d48388c129ff049
craigmiller160 <craigmiller160@gmail.com>
2018-03-05 16:19:21 -0800

    Added ingredients to recipe

--- src/app/service/recipe.service.ts
+++ src/app/service/recipe.service.ts
@@ -1,12 +1,22 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from '../model/recipe.model';
+import { Ingredient } from '../model/ingredient.model';
 
 @Injectable()
 export class RecipeService {
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe #1', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe #2', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Tasty Schnitzel', 'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger', 'What else do you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg', [
+        new Ingredient('Meat', 1),
+        new Ingredient('Buns', 2),
+        new Ingredient('Cheese', 1)
+      ])
   ];
   readonly recipeSelected = new EventEmitter<Recipe>();
 
