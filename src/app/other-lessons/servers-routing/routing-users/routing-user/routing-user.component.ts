d9c4c2060e4a745c04a9dc61459b2f12158e6e74
craigmiller160 <craigmiller160@gmail.com>
2018-03-09 11:08:39 -0800

    Looked at unsubscribing from router changes

--- src/app/other-lessons/servers-routing/routing-users/routing-user/routing-user.component.ts
+++ src/app/other-lessons/servers-routing/routing-users/routing-user/routing-user.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-routing-user',
   templateUrl: './routing-user.component.html',
   styleUrls: ['./routing-user.component.css']
 })
-export class RoutingUserComponent implements OnInit {
+export class RoutingUserComponent implements OnInit, OnDestroy {
 
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,7 +19,13 @@ export class RoutingUserComponent implements OnInit {
       id: this.route.snapshot.params.id,
       name: this.route.snapshot.params.name
     };
-    this.route.params.subscribe(params => this.user = {id: params.id, name: params.name});
+    // Only necessary if the component should be re-created while having been already loaded
+    this.paramsSubscription = this.route.params.subscribe(params => this.user = {id: params.id, name: params.name});
+  }
+
+  ngOnDestroy(): void {
+    // Not necessary because angular handles cleaning up router subscriptions. Only necessary for other observables
+    this.paramsSubscription.unsubscribe();
   }
 
 }
