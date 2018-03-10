f830d5431380985e868fac1914cd906c0e553c2e
Craig Miller <craigmiller160@gmail.com>
2018-03-10 09:24:00 -0500

    Applied the can activate guards to the children, and implemented fake auth service

--- src/app/other-lessons/servers-routing/routing-home/routing-home.component.ts
+++ src/app/other-lessons/servers-routing/routing-home/routing-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-routing-home',
@@ -8,7 +9,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class RoutingHomeComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -16,6 +17,14 @@ export class RoutingHomeComponent implements OnInit {
   onLoadServer(id: number): void {
     // complex calculation stuff
     this.router.navigate(['../servers', id, 'edit'], {relativeTo: this.route, queryParams: {allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin(): void {
+    this.authService.login();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
   }
 
 }
