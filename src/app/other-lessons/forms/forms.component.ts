a9175de9aad4a3be4be00494b117798bd5667d95
craigmiller160 <craigmiller160@gmail.com>
2018-03-18 08:47:08 -0400

    Added some more binding to the form

--- src/app/other-lessons/forms/forms.component.ts
+++ src/app/other-lessons/forms/forms.component.ts
@@ -9,6 +9,8 @@ import { NgForm } from '@angular/forms';
 export class FormsComponent implements OnInit {
 
   @ViewChild('f') form: NgForm;
+  defaultQuestion = 'pet';
+  answer: string;
 
   constructor() { }
 
